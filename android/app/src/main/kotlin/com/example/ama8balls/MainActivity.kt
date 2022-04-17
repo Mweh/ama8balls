92e08f7c9ae83726d56b1e4518d7cad731a0d3d8
Mweh <40236619+Mweh@users.noreply.github.com>
2022-04-17 17:59:07 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ama8balls/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ama8balls
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int BallNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {
+            setState(() {
+              BallNumber = Random().nextInt(5) + 1;
+              print(BallNumber);
+            });
+          },
+          child: Image.asset('images/ball$BallNumber.png'),
+        )
+      ],
+    );
+  }
+}
